ad4f8111c926ad80331b6532d0cf7b9265208ec0
jy-whatthetech <james.yeh1@gmail.com>
2020-12-02 22:55:18 -0800

    day 3 part 2

--- day3.py
+++ day3.py
@@ -1,23 +1,44 @@
-f = open("d3.txt", "r")
 
-f.readline()
-line = f.readline()
-width = len(line.strip())
-print(width)
 
-offset = 3
-res = 0
+sofar = 1
 
-while line and len(line) > 0:
-    ind = offset % width
-    c = line[ind]
-    # print(c)
-    if c == "#":
-        res += 1
+right = 3
+down = 1
+
+for right, down in ([3,1], [1,1], [5,1], [7,1], [1,2]):
+    f = open("d3.txt", "r")
+
+    for i in range(0, down):
+        f.readline()
     line = f.readline()
-    offset += 3
+    width = len(line.strip())
 
-f.close()
+    offset = right
+    res = 0
 
-print(res)
+    while line and len(line) > 0:
+        ind = offset % width
+        c = line[ind]
+        # print(c)
+        if c == "#":
+            res += 1
+        
+        normal = True
+        for i in range(1, down):
+            temp = f.readline()
+            if not temp or not len(temp):
+                normal = False
+                break
+
+        if not normal:
+            break
+
+        line = f.readline()
+        offset += right
+
+    f.close()
+
+    sofar *= res
+
+print(sofar)
 
