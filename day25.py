8ac3372d22702d66c842bc395c3c6a523f271682
jy-whatthetech <james.yeh1@gmail.com>
2020-12-25 15:11:09 -0800

    day 25 (last day!)

---
+++ day25.py
@@ -0,0 +1,27 @@
+from collections import deque
+f = open("d25.txt", "r")
+
+lineArr = f.read().strip().split("\n")
+
+f.close()
+
+pubKey1, pubKey2 = [int(x) for x in lineArr]
+
+def getLoopSize(subject, pubKey):
+    loop = 0
+    curr = 1
+    while True:
+        if curr == pubKey:
+            return loop
+        loop += 1
+        curr = (curr * subject) % 20201227
+
+def getEncKey(subject, loopSize):
+    curr = 1
+    for x in range(0, loopSize):
+        curr = (curr * subject) % 20201227
+    return curr
+
+part1 = getEncKey(pubKey2, getLoopSize(7, pubKey1))
+
+print(part1)
